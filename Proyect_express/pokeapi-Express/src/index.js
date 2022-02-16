bdcce10f12d0f262dbeaaf81203c41f3662c6cb6
Daniel Isai <daisai1228@hotmail.com>
2022-02-16 12:58:23 -0600

    feat:My server listen port 3000

--- Proyect_express/pokeapi-Express/src/index.js
+++ Proyect_express/pokeapi-Express/src/index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
+const port = 3000
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
+app.use(express.json())
+
+app.listen(port, () => {
+    console.log(`Server ok, port: ${port}`);
 })
-
-app.listen(3000)

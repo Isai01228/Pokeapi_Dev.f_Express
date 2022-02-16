60c8bf6da0f06e36d77174d0e5f546e85dc1caed
Daniel Isai <daisai1228@hotmail.com>
2022-02-16 15:32:07 -0600

    feat:add my routes

---
+++ Proyect_express/pokeapi-Express/src/routes/pokemones/pokemones.router.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const pokemonRouter = express.Router();
+const PokemonesServices = require('../../services/pokemones/pokemones.services');
+
+const servicePokemonObject = new PokemonesServices()
+
+pokemonRouter.get("/", async (req, res) => {
+	const data = await servicePokemonObject.getAll()
+	res.json({data});
+})
+
+module.exports = pokemonRouter
